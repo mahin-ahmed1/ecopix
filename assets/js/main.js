e5488a01d022d7288ec247553f148e65ad8e9416
Retype GitHub Action <retype@object.net>
2023-11-29 09:20:58 +0000

    Refreshes Retype-generated documentation.
    Process triggered by mahin-ahmed1.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,13 @@ jQuery(document).ready(function($) {
     $('.menu-item-has-children').children('a').append("<a class='submenu-indicator' href='#'>+</a>");
 
     $(".submenu-indicator").click(function(){
-      $(this).closest('li').find('.sub-menu').first().toggleClass('hehe');
+      $(this).closest('li').find('.sub-menu').first().toggleClass('active-submenu');
 
     });
+
+    $('.toggle-menu').click(function(){
+      $('.menu-area .navbar').slideToggle(400);
+      return false;
+    
+     })
   });
